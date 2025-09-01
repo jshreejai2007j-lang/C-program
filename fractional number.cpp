7963307aec3380676af508f0383b4a1fdb813e82
jshreejai2007j-lang <j.shreejai2007.j@gmail.com>
2025-09-01 11:57:19 +0530

    Add files via upload

---
+++ fractional number.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	float num=1.567;
+	printf("fractional number is %f",num);
+	return 0;
+}
---
+++ welcome.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	char a;
+	scanf("%s",&a);
+	printf("%s,welcome");
+	return 0;
+}
