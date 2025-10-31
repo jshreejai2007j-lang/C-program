a77041d91b024a15d3edde6f9dbd25d6d56c9433
jshreejai2007j-lang <j.shreejai2007.j@gmail.com>
2025-10-31 21:48:57 +0530

    Add files via upload

---
+++ a,b,c or fail.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int marks;
+    printf("Enter your marks: ");
+    scanf("%d", &marks);
+    if (marks >= 75) {
+        printf("Grade: A\n");
+    } else if (marks >= 50) {
+        printf("Grade: B\n");
+    } else if (marks >= 35) {
+        printf("Grade: C\n");
+    } else {
+        printf("Grade: Fail\n");
+    }
+    
+    return 0;
+}
